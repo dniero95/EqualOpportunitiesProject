466851740132233654a7c7218ad4c905882bcd30
dniero95 <darioniero@icloud.com>
2022-11-09 12:17:34 +0100

    initial commit

---
+++ README.md
@@ -0,0 +1,4 @@
+# Equal Opportunities Project
+
+A Pen created on CodePen.io. Original URL: [https://codepen.io/Borgiak/pen/xxXgZRz](https://codepen.io/Borgiak/pen/xxXgZRz).
+
---
+++ script.js
@@ -0,0 +1,24 @@
+function dario() {
+    document.getElementById("intervento-Dario").innerHTML = "Cose sensate che dice Dario ";
+    document.getElementById("intervento-Marika").innerHTML = "";
+    document.getElementById("intervento-Damira").innerHTML = "";
+    document.getElementById("intervento-Andrei").innerHTML = "";
+  }
+  function marika() {
+    document.getElementById("intervento-Marika").innerHTML = "Cose sensate che dice Marika ";
+    document.getElementById("intervento-Dario").innerHTML = "";
+    document.getElementById("intervento-Damira").innerHTML = "";
+    document.getElementById("intervento-Andrei").innerHTML = "";
+  }
+  function damira() {
+    document.getElementById("intervento-Damira").innerHTML = "Cose sensate che dice  Damira";
+    document.getElementById("intervento-Marika").innerHTML = "";
+    document.getElementById("intervento-Dario").innerHTML = "";
+    document.getElementById("intervento-Andrei").innerHTML = "";
+  }
+  function andrei() {
+    document.getElementById("intervento-Andrei").innerHTML = "Cose sensate che dice  Andrei";
+    document.getElementById("intervento-Marika").innerHTML = "";
+    document.getElementById("intervento-Damira").innerHTML = "";
+    document.getElementById("intervento-Dario").innerHTML = "";
+  }
